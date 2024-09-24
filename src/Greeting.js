4fe3b1c1a427dc145dbac1927f188a1f195c7743
Talal Ilyas <talalilyas11@gmail.com>
2024-09-24 17:38:39 +0500

    Update Greeting.js

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,214 +1,73 @@
 import { useLocation } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< Updated upstream
-export default function Greeting() {
-  const location = useLocation();
-  const { firstName, lastName, email , password ,   selectedOption ,selectyear ,selectday , gender} = location.state || {};
-=======
 import Navigationlink from "./Navigationlink";
 import UserDetails from "./UserDetails";
-import Username from "./Username";
 
 export default function Greeting() {
   const location = useLocation();
-  const {} = location.state || {};
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    selectedOption,
+    selectyear,
+    selectday,
+    gender,
+  } = location.state || {};
 
->>>>>>> Stashed changes
   return (
-    <div class="container-fluid">
-      <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-          <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <a
               href="/"
-              class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+              className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
             >
-              <span class="fs-5 d-none d-sm-inline">Menu</span>
+              <span className="fs-5 d-none d-sm-inline">Menu</span>
             </a>
-<<<<<<< Updated upstream
-            <ul
-              class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-              id="menu"
-            >
-              <li class="nav-item">
-                <a>
-                  <i class="fs-4 bi-house"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Home</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#submenu1"
-                  data-bs-toggle="collapse"
-                  class="nav-link px-0 align-middle"
-                >
-                  <i class="fs-4 bi-speedometer2"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Dashboard</span>
-                </a>
-                <ul
-                  class="collapse show nav flex-column ms-1"
-                  id="submenu1"
-                  data-bs-parent="#menu"
-                >
-                  <li class="w-100">
-                    <a  class=" px-0">
-              
-                      <span class="d-none d-sm-inline">Item</span> 1
-                    </a>
-                  </li>
-                  <li>
-                    <a class="px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Item</span> 2
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a  class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-table"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Orders</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#submenu2"
-                  data-bs-toggle="collapse"
-                  class="nav-link px-0 align-middle "
-                >
-                  <i class="fs-4 bi-bootstrap"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Bootstrap</span>
-                </a>
-                <ul
-                  class="collapse nav flex-column ms-1"
-                  id="submenu2"
-                  data-bs-parent="#menu"
-                >
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0">
-                    
-                      <span class="d-none d-sm-inline">Item</span> 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Item</span> 2
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a
-                  href="#submenu3"
-                  data-bs-toggle="collapse"
-                  class="nav-link px-0 align-middle"
-                >
-                  <i class="fs-4 bi-grid"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Products</span>{" "}
-                </a>
-                <ul
-                  class="collapse nav flex-column ms-1"
-                  id="submenu3"
-                  data-bs-parent="#menu"
-                >
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 1
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 2
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 3
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0">
-                      {" "}
-                      <span class="d-none d-sm-inline">Product</span> 4
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-people"></i>{" "}
-                  <span class="ms-1 d-none d-sm-inline">Customers</span>{" "}
-                </a>
-              </li>
-            </ul>
-
-            <div class="dropdown pb-4">
+            <Navigationlink />
+            <div className="dropdown pb-4">
               <a
                 href="#"
-                class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
                 id="dropdownUser1"
                 data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
+                aria-expanded="false">
                 <img
                   src="https://github.com/mdo.png"
                   alt="hugenerd"
                   width="30"
                   height="30"
-                  class="rounded-circle"
+                  className="rounded-circle"
                 ></img>
-                <span class="d-none d-sm-inline mx-1">{firstName} {lastName}</span>
+                <span className="d-none d-sm-inline mx-1">
+                  {firstName} {lastName}
+                </span>
               </a>
-
-              <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    New project...
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Profile
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider"></hr>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Sign out
-                  </a>
-                </li>
+              <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+                <li><a className="dropdown-item" href="#">New project...</a></li>
+                <li><a className="dropdown-item" href="#">Settings</a></li>
+                <li><a className="dropdown-item" href="#">Profile</a></li>
+                <li className="dropdown-divider"> </li>
+                <li><a className="dropdown-item" href="#">Sign out</a></li>
               </ul>
             </div>
           </div>
         </div>
-        <div class="col py-3">
-
-          <h4>User Email</h4> {email}
-          <h4>User password</h4>{ password}
-          <h4>User birthdaymonth</h4>{selectedOption}
-          <h4>User birthyear</h4>{selectyear}
-          <h4>User birthday</h4>{selectday}
-          <h4>User birthday</h4>{gender}
-=======
-            <Navigationlink />
-            <Username state={location.state || {}} />
-          </div>
-        </div>
         <div className="col py-3">
-          <UserDetails state={location.state || {}} />
->>>>>>> Stashed changes
+          <UserDetails
+            firstName={firstName}
+            lastName={lastName}
+            email={email}
+            password={password}
+            selectedOption={selectedOption}
+            selectyear={selectyear}
+            selectday={selectday}
+            gender={gender}
+          />
         </div>
       </div>
     </div>
