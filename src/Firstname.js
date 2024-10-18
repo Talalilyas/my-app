03c8b28b75850e85ef7694cbd4d875fb4ca4f02b
Talal Ilyas <talalilyas11@gmail.com>
2024-10-18 19:52:19 +0500

    clearing the compenets #1

--- src/Firstname.js
+++ src/Firstname.js
@@ -1,24 +1,24 @@
-export default function Fristname(props) {
-  const onChange = (event) => {
-    const valuess = event.target.value;
-    console.log(valuess);
-    props.onChange(valuess, props.name);
-  };
-  return (
-    <>
-      <form>
-        <div>
-          <label for="firstName" class="form-label"></label>
-          <input
-            type={props.type}
-            className={props.className}
-            id="firstName"
-            onChange={onChange}
-            value={props.value}
-            placeholder={props.placeholder}
-          ></input>
-        </div>
-      </form>
-    </>
-  );
-}
+// // export default function Fristname(props) {
+// //   const onChange = (event) => {
+// //     const valuess = event.target.value;
+// //     console.log(valuess);
+// //     props.onChange(valuess, props.name);
+// //   };
+// //   return (
+// //     <>
+// //       <form>
+// //         <div>
+// //           <label for="firstName" class="form-label"></label>
+// //           <input
+// //             type={props.type}
+// //             className={props.className}
+// //             id="firstName"
+// //             onChange={onChange}
+// //             value={props.value}
+// //             placeholder={props.placeholder}
+// //           ></input>
+// //         </div>
+// //       </form>
+// //     </>
+//   );
+// }
