03699ac1843a98f3b088adba364b84112997c8f9
Talal Ilyas <talalilyas11@gmail.com>
2024-10-24 19:04:41 +0500

    some update on the sginup button

--- src/NewHeader.js
+++ src/NewHeader.js
@@ -1,22 +1,42 @@
 import { Link, useLocation } from "react-router-dom";
 import React from "react";
-import { Button, Flex } from "antd";
+import { Button, Layout, Menu } from "antd";
+import {PoweroffOutlined } from "@ant-design/icons";
+
+const { Header } = Layout;
 
 export default function NewHeader() {
   const location = useLocation();
 
+  // Only show header on the home page "/"
+  if (location.pathname !== "/") {
+    return null;
+  }
+
   return (
-    <div>
-      {location.pathname === "/" && (
-        <div className="col-md-12 t">
-          <Link to="/NewHeader">
-            <Flex gap="small" wrap>
-              <Button type="primary">Sign in</Button>
-            </Flex>
-          </Link>
-         
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" style={{ color: "white", fontSize: "20px" }}>
+          
         </div>
-      )}
-    </div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+         
+          
+          <div style={{ float: "right" }}>
+            <Link to="/NewHeader">
+              <Button
+                type="primary"
+                icon={<PoweroffOutlined />}
+                style={{ marginRight: "10px" }}
+              >
+                Sign in
+              </Button>
+            </Link>
+
+            
+          </div>
+        </Menu>
+      </Header>
+    </Layout>
   );
 }
