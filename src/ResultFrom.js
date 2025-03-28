b62e1bb09ea9eac95700493636dc479d07db5bc4
Talal Ilyas <talalilyas11@gmail.com>
2025-03-29 03:15:54 +0500

    Add:new adding the teacher

--- src/ResultFrom.js
+++ src/ResultFrom.js
@@ -3,44 +3,38 @@ import { Form, Input, Button, Row, Col, Select, InputNumber } from "antd";
 
 export default function ResultForm() {
   const [value, setValue] = useState(3);
-
+  const [form] = Form.useForm(); 
   function handleChange(val) {
     setValue(val);
   }
-
   const handleChangeSelect = (val) => {
-    console.log(`Selected: ${val}`);
+    const teacherName = val === "lucy" ? "Prof. Amjad" : "Ahmad";
+    form.setFieldsValue({ teacher: teacherName }); 
   };
-
   const handleFormSubmit = (values) => {
     console.log("Form Submitted:", values);
   };
-
   return (
     <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
       <Col xs={24} sm={18} md={12} lg={8}>
         <h3>Student Result Card</h3>
-        <Form layout="vertical" onFinish={handleFormSubmit}>
+        <Form form={form} layout="vertical" onFinish={handleFormSubmit}>
           <Form.Item
             label="First Name"
             name="firstName"
             rules={[
               { required: true, message: "Please enter your first name!" },
-            ]}
-          >
+            ]}>
             <Input placeholder="Enter your first name" />
           </Form.Item>
-
           <Form.Item
             label="Last Name"
             name="lastName"
             rules={[
               { required: true, message: "Please enter your last name!" },
-            ]}
-          >
+            ]}>
             <Input placeholder="Enter your last name" />
           </Form.Item>
-
           <Row gutter={16}>
             <Col span={8}>
               <Form.Item
@@ -48,8 +42,7 @@ export default function ResultForm() {
                 name="classNumber"
                 rules={[
                   { required: true, message: "Please enter your class!" },
-                ]}
-              >
+                ]}>
                 <InputNumber
                   min={1}
                   max={20}
@@ -59,15 +52,13 @@ export default function ResultForm() {
                 />
               </Form.Item>
             </Col>
-
             <Col span={16}>
               <Form.Item
                 label="Result Type"
                 name="resultType"
                 rules={[
                   { required: true, message: "Please select a result type!" },
-                ]}
-              >
+                ]} >
                 <Select
                   placeholder="Select Result Type"
                   onChange={handleChangeSelect}
@@ -79,6 +70,15 @@ export default function ResultForm() {
               </Form.Item>
             </Col>
           </Row>
+          <Form.Item
+            label="Teacher"
+            name="teacher"
+            rules={[
+              { required: true, message: "Please select a result type!" },
+            ]}
+          >
+            <Input placeholder="Teacher Name" disabled />
+          </Form.Item>
 
           <Form.Item>
             <Button type="primary" htmlType="submit">
