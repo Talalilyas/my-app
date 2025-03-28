e8ce26439afbe6b66ea8f49a0ec9e74f85ed3969
Talal Ilyas <talalilyas11@gmail.com>
2025-03-29 02:52:22 +0500

    Fix :devlop the responsive to all device

--- src/ResultFrom.js
+++ src/ResultFrom.js
@@ -1,66 +1,84 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Row, Col, Select, Space, InputNumber } from "antd";
+import { Form, Input, Button, Row, Col, Select, InputNumber } from "antd";
 
 export default function ResultForm() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
   const [value, setValue] = useState(3);
+
   function handleChange(val) {
     setValue(val);
-    console.log("InputNumber value:", val);
   }
+
   const handleChangeSelect = (val) => {
     console.log(`Selected: ${val}`);
   };
+
   const handleFormSubmit = (values) => {
     console.log("Form Submitted:", values);
   };
 
   return (
     <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
-      <Col xs={24} sm={18} md={12} lg={6}>
+      <Col xs={24} sm={18} md={12} lg={8}>
         <h3>Student Result Card</h3>
         <Form layout="vertical" onFinish={handleFormSubmit}>
           <Form.Item
             label="First Name"
             name="firstName"
-            rules={[{ required: true, message: "Please enter your first name!" }]}
+            rules={[
+              { required: true, message: "Please enter your first name!" },
+            ]}
           >
-            <Input
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              placeholder="Enter your first name"
-            />
+            <Input placeholder="Enter your first name" />
           </Form.Item>
+
           <Form.Item
             label="Last Name"
             name="lastName"
-            rules={[{ required: true, message: "Please enter your last name!" }]}
+            rules={[
+              { required: true, message: "Please enter your last name!" },
+            ]}
           >
-            <Input
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              placeholder="Enter your last name"
-            />
+            <Input placeholder="Enter your last name" />
           </Form.Item>
-          <Col span={12} lg={24}>
-          <Form.Item label="Enter class">
-            <InputNumber min={1} max={20} defaultValue={3} onChange={handleChange}  style={{width :"40%"}}/>
-            <Select
-              defaultValue="lucy"
-              style={{ width:"60%"}}
-              onChange={handleChangeSelect}
-              options={[
-                { value: "jack", label: "Final result" },
-                { value: "lucy", label: "Semster system" },
-               
-              ]}
-            />
-          </Form.Item>
-          </Col>
-          {value < 12 ? <p>Final Result</p> : <p>Semester System</p>}
-         
-          
+
+          <Row gutter={16}>
+            <Col span={8}>
+              <Form.Item
+                label="Enter Class"
+                name="classNumber"
+                rules={[
+                  { required: true, message: "Please enter your class!" },
+                ]}
+              >
+                <InputNumber
+                  min={1}
+                  max={20}
+                  value={value}
+                  onChange={handleChange}
+                  style={{ width: "100%" }}
+                />
+              </Form.Item>
+            </Col>
+
+            <Col span={16}>
+              <Form.Item
+                label="Result Type"
+                name="resultType"
+                rules={[
+                  { required: true, message: "Please select a result type!" },
+                ]}
+              >
+                <Select
+                  placeholder="Select Result Type"
+                  onChange={handleChangeSelect}
+                  options={[
+                    { value: "jack", label: "Final result" },
+                    { value: "lucy", label: "Semester system" },
+                  ]}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
 
           <Form.Item>
             <Button type="primary" htmlType="submit">
