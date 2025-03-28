39ce61fdeda647b66b68c35a764e7a024b6a7b83
Talal Ilyas <talalilyas11@gmail.com>
2025-03-28 15:32:50 +0500

    ADD:fix some update in this

--- src/ResultFrom.js
+++ src/ResultFrom.js
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Row, Col } from "antd";
+import { Form, Input, Button, Row, Col, Select, Space, InputNumber } from "antd";
 
-export default function ResultFrom() {
+export default function ResultForm() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [value, setValue] = useState(3);
 
+  // Handle number input change
+  function handleChange(val) {
+    setValue(val);
+    console.log("InputNumber value:", val);
+  }
+
+  // Handle select change
+  const handleChangeSelect = (val) => {
+    console.log(`Selected: ${val}`);
+  };
+
+  // Handle form submission
   const handleFormSubmit = (values) => {
     console.log("Form Submitted:", values);
   };
@@ -12,13 +25,12 @@ export default function ResultFrom() {
   return (
     <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
       <Col xs={24} sm={18} md={12} lg={6}>
+        <h3>Student Result Card</h3>
         <Form layout="vertical" onFinish={handleFormSubmit}>
           <Form.Item
             label="First Name"
             name="firstName"
-            rules={[
-              { required: true, message: "Please enter your first name!" },
-            ]}
+            rules={[{ required: true, message: "Please enter your first name!" }]}
           >
             <Input
               value={firstName}
@@ -30,15 +42,30 @@ export default function ResultFrom() {
           <Form.Item
             label="Last Name"
             name="lastName"
-            rules={[
-              { required: true, message: "Please enter your last name!" },
-            ]}
+            rules={[{ required: true, message: "Please enter your last name!" }]}
           >
             <Input
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
               placeholder="Enter your last name"
             />
+          </Form.Item>
+
+          <Form.Item label="Select Result type">
+            <Select
+              defaultValue="lucy"
+              style={{ width: 120 }}
+              onChange={handleChangeSelect}
+              options={[
+                { value: "jack", label: "Final result" },
+                { value: "lucy", label: "Semster system" },
+               
+              ]}
+            />
+          </Form.Item>
+
+          <Form.Item label="Enter Number">
+            <InputNumber min={1} max={20} defaultValue={3} onChange={handleChange} />
           </Form.Item>
 
           <Form.Item>
